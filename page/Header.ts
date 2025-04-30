4deab2c18585a418f0fc134807e404de7bcdced9
Felipanjos <61696148+Felipanjos@users.noreply.github.com>
2025-04-30 09:32:40 -0300

    de-coupling navigation from header

--- page/Header.ts
+++ page/Header.ts
@@ -8,16 +8,6 @@ export class Header {
     this.page = page;
 
     this.selectors = {
-      deleteAccount: page.getByRole('link', { name: 'Delete Account' }),
-      contactUs: page.getByRole('link', { name: 'Contact us' }),
-      home: page.getByRole('link', { name: 'Home' }),
-      testCases: page.getByRole('listitem').filter({has: page.getByRole('link', { name: 'Test Cases' })}),
-      products: page.getByRole('listitem').filter({has: page.getByRole('link', { name: 'Products' })}),
-      cart: page.getByRole('listitem').filter({has: page.getByRole('link', { name: 'Cart' })}),
     };
-  }
-
-  getLoggedIn(name) {
-    return this.page.getByText(`Logged in as ${name}`);
   }
 }
--- page/Navigation.ts
+++ page/Navigation.ts
@@ -8,6 +8,12 @@ export class Navigation {
     this.page = page;
     this.selectors = {
       signUpAndLogin: this.page.getByRole('link', { name: 'Signup / Login' }),
+      deleteAccount: page.getByRole('link', { name: 'Delete Account' }),
+      contactUs: page.getByRole('link', { name: 'Contact us' }),
+      home: page.getByRole('link', { name: 'Home' }),
+      testCases: page.getByRole('listitem').filter({has: page.getByRole('link', { name: 'Test Cases' })}),
+      products: page.getByRole('listitem').filter({has: page.getByRole('link', { name: 'Products' })}),
+      cart: page.getByRole('listitem').filter({has: page.getByRole('link', { name: 'Cart' })}),
     }
   }
 
@@ -15,7 +21,15 @@ export class Navigation {
     await this.page.goto('http://automationexercise.com/');
   }
 
+  async loginPage() {
+    await this.selectors.signUpAndLogin.click();
+  }
+
   async sign() {
     await this.selectors.signUpAndLogin.click();
+  }
+
+  getLoggedIn(name) {
+    return this.page.getByText(`Logged in as ${name}`);
   }
 }
